6c63b2fb3a12acdbd7610ea387d5803c349abfd9
tangjian <tangjian1891@163.com>
2025-04-14 13:51:51 +0800

    ✨ feat:采用grid渲染表格

--- src/main.ts
+++ src/main.ts
@@ -5,34 +5,38 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import VxeUITable from 'vxe-table'
+import VxeUITable, { VxeUI } from 'vxe-table'
 import 'vxe-table/lib/style.css'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import test from './test.vue'
 const app = createApp(App)
-
-VxeUITable.VxeUI.formats.add('mySwitch', {
-  tableCellFormatMethod({ cellValue }) {
-    return cellValue === 1 ? '开启' : '关闭'
-  },
-})
-
-VxeUITable.VxeUI.formats.mixin({
-  myAmount: {
-    tableCellFormatMethod({ cellValue }, ...params) {
-      console.log(cellValue, params)
-
-      return '什么鬼东西'
-    },
-  },
-})
 
 VxeUITable.VxeUI.renderer.add('MyTableCellLink', {
   // 默认显示模板
   renderTableDefault(renderOpts, renderParams) {
     return h('span', {}, '什么啊12312123')
+  },
+})
+
+VxeUI.renderer.add('setStyle', {
+  tableCellClassName() {
+    console.log('没有吗啊')
+
+    return 'redredredredredred'
+  },
+  tableCellStyle() {
+    return { color: 'red', background: 'blue' }
+  },
+  renderTableDefault() {
+    return '分分分'
+  },
+})
+
+VxeUI.renderer.add('MyTableFilterInput', {
+  renderTableHeader() {
+    return h('span', {}, '表头')
   },
 })
 app.use(createPinia())
