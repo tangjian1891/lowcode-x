c85e504bc09ee291db886377f303bcb20f4a1b79
tangjian <tangjian1891@163.com>
2025-04-22 14:56:50 +0800

    🐞 fix:两栏逻辑完成

--- src/components/jas-resize/jas-resize-bar.vue
+++ src/components/jas-resize/jas-resize-bar.vue
@@ -1,23 +1,18 @@
 <template>
   <div
     class="quick_btn"
-    :style="{ 'flex-basis': barWidth + 'px' }"
+    :style="{ width: barWidth + 'px' }"
     @mousedown.prevent="emit('mousedown', $event)"
-    @mouseenter="isHovered = true"
-    @mouseleave="isHovered = false"
   >
     <!-- 中心常驻快捷按钮 -->
-    <div class="toggle-button" @click.stop="toggleSections">
+    <div class="toggle-button" @click.stop="toggleSections" @mousedown.prevent>
       <span class="toggle-icon" :class="{ collapsed: leftWidth === 0 }"></span>
     </div>
-    <!-- 鼠标悬停时显示的阴影区域 -->
-    <div class="hover-shadow" :class="{ active: isHovered }"></div>
+    <div :class="['extra', { active: isActive }]" @mouseover="mouseover" @mouseout="mouseout"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-
 const props = defineProps({
   barWidth: {
     type: Number,
@@ -26,9 +21,6 @@ const props = defineProps({
   leftWidth: Number,
 })
 
-const isHovered = ref(false)
-const isCollapsed = ref(false)
-
 const emit = defineEmits(['mousedown', 'toggle'])
 
 // 处理快捷按钮点击事件
@@ -36,26 +28,52 @@ const toggleSections = (event) => {
   event.stopPropagation() // 阻止事件冒泡
   emit('toggle')
 }
+
+const isActive = ref(false)
+function mouseover() {
+  clearTimeout(timeSeed)
+  isActive.value = true
+}
+let timeSeed
+function mouseout() {
+  clearTimeout(timeSeed)
+  timeSeed = setTimeout(() => {
+    isActive.value = false
+  }, 300)
+}
 </script>
 
 <style lang="scss" scoped>
 .quick_btn {
+  position: relative;
   cursor: move;
   flex-shrink: 0;
   background-color: black;
   position: relative;
-  transition: background-color 0.2s;
+  .extra {
+    width: 20px;
+    background-color: transparent;
+    height: 100%;
+    transition: all 0.3s ease;
+    opacity: 0;
+    position: absolute;
+    right: -20px;
+    top: 0;
+    box-shadow: none;
+  }
 
-  &:hover {
-    background-color: #f0f0f0;
+  .extra.active {
+    opacity: 1;
+    background-color: rgba(245, 245, 245, 0.8);
+    box-shadow: 4px 0 10px rgba(0, 0, 0, 0.1);
   }
 
   // 常驻的快捷按钮
   .toggle-button {
     position: absolute;
     top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    left: 0;
+    transform: translate(0, -50%);
     width: 16px;
     height: 16px;
     border-radius: 50%;
@@ -86,24 +104,6 @@ const toggleSections = (event) => {
       &.collapsed {
         transform: rotate(180deg);
       }
-    }
-  }
-
-  // 鼠标悬停时扩大的阴影区域
-  .hover-shadow {
-    position: absolute;
-    top: 0;
-    left: -5px; // 向左扩展5px
-    right: -5px; // 向右扩展5px
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.05);
-    opacity: 0;
-    transition: opacity 0.2s;
-    pointer-events: none; // 不影响鼠标事件
-    z-index: 1;
-
-    &.active {
-      opacity: 1;
     }
   }
 }
